6a94ea064edc9b874c332c6555109133ab51b556
salvadorp2001 <salvadorp2001@gmail.com>
2015-04-23 20:14:33 -0300

    First Public Version

---
+++ Inventory_app/Form1.Designer.cs
@@ -0,0 +1,344 @@
+﻿namespace Inventory_app
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.numericUpDown5 = new System.Windows.Forms.NumericUpDown();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.numericUpDown6 = new System.Windows.Forms.NumericUpDown();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.numericUpDown7 = new System.Windows.Forms.NumericUpDown();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.numericUpDown8 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown9 = new System.Windows.Forms.NumericUpDown();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.numericUpDown10 = new System.Windows.Forms.NumericUpDown();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.numericUpDown11 = new System.Windows.Forms.NumericUpDown();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown11)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(13, 52);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(30, 20);
+            this.numericUpDown1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(50, 52);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(222, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(50, 78);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(222, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(13, 78);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(30, 20);
+            this.numericUpDown2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(50, 104);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(222, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(13, 104);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(30, 20);
+            this.numericUpDown3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(50, 130);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(222, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(13, 130);
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(30, 20);
+            this.numericUpDown4.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 368);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 50);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(147, 368);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 50);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Load";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(50, 234);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(222, 20);
+            this.textBox5.TabIndex = 23;
+            // 
+            // numericUpDown5
+            // 
+            this.numericUpDown5.Location = new System.Drawing.Point(13, 234);
+            this.numericUpDown5.Name = "numericUpDown5";
+            this.numericUpDown5.Size = new System.Drawing.Size(30, 20);
+            this.numericUpDown5.TabIndex = 22;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(50, 208);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(222, 20);
+            this.textBox6.TabIndex = 21;
+            // 
+            // numericUpDown6
+            // 
+            this.numericUpDown6.Location = new System.Drawing.Point(13, 208);
+            this.numericUpDown6.Name = "numericUpDown6";
+            this.numericUpDown6.Size = new System.Drawing.Size(30, 20);
+            this.numericUpDown6.TabIndex = 20;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(50, 182);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(222, 20);
+            this.textBox7.TabIndex = 19;
+            // 
+            // numericUpDown7
+            // 
+            this.numericUpDown7.Location = new System.Drawing.Point(13, 182);
+            this.numericUpDown7.Name = "numericUpDown7";
+            this.numericUpDown7.Size = new System.Drawing.Size(30, 20);
+            this.numericUpDown7.TabIndex = 18;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(50, 156);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(222, 20);
+            this.textBox8.TabIndex = 17;
+            // 
+            // numericUpDown8
+            // 
+            this.numericUpDown8.Location = new System.Drawing.Point(13, 156);
+            this.numericUpDown8.Name = "numericUpDown8";
+            this.numericUpDown8.Size = new System.Drawing.Size(30, 20);
+            this.numericUpDown8.TabIndex = 16;
+            // 
+            // numericUpDown9
+            // 
+            this.numericUpDown9.Location = new System.Drawing.Point(13, 261);
+            this.numericUpDown9.Name = "numericUpDown9";
+            this.numericUpDown9.Size = new System.Drawing.Size(30, 20);
+            this.numericUpDown9.TabIndex = 24;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(50, 260);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(222, 20);
+            this.textBox9.TabIndex = 25;
+            // 
+            // numericUpDown10
+            // 
+            this.numericUpDown10.Location = new System.Drawing.Point(13, 288);
+            this.numericUpDown10.Name = "numericUpDown10";
+            this.numericUpDown10.Size = new System.Drawing.Size(30, 20);
+            this.numericUpDown10.TabIndex = 26;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(50, 288);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(221, 20);
+            this.textBox10.TabIndex = 27;
+            // 
+            // numericUpDown11
+            // 
+            this.numericUpDown11.Location = new System.Drawing.Point(13, 315);
+            this.numericUpDown11.Name = "numericUpDown11";
+            this.numericUpDown11.Size = new System.Drawing.Size(30, 20);
+            this.numericUpDown11.TabIndex = 28;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(50, 315);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(221, 20);
+            this.textBox11.TabIndex = 29;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(259, 33);
+            this.button3.TabIndex = 30;
+            this.button3.Text = "About";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(13, 342);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(262, 20);
+            this.textBox12.TabIndex = 31;
+            this.textBox12.Text = "File Destination";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(287, 422);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.numericUpDown11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.numericUpDown10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.numericUpDown9);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.numericUpDown5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.numericUpDown6);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.numericUpDown7);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.numericUpDown8);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Inventory";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown11)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.NumericUpDown numericUpDown5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.NumericUpDown numericUpDown6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.NumericUpDown numericUpDown7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.NumericUpDown numericUpDown8;
+        private System.Windows.Forms.NumericUpDown numericUpDown9;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.NumericUpDown numericUpDown10;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.NumericUpDown numericUpDown11;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox12;
+    }
+}
+
---
+++ Inventory_app/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inventory_app
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] to_write = { (Convert.ToString(numericUpDown1.Value)), textBox1.Text, (Convert.ToString(numericUpDown2.Value)), textBox2.Text, (Convert.ToString(numericUpDown3.Value)), textBox3.Text, (Convert.ToString(numericUpDown4.Value)), textBox4.Text, (Convert.ToString(numericUpDown5.Value)), textBox5.Text, (Convert.ToString(numericUpDown6.Value)), textBox6.Text, (Convert.ToString(numericUpDown7.Value)), textBox7.Text, (Convert.ToString(numericUpDown8.Value)), textBox8.Text, (Convert.ToString(numericUpDown9.Value)),textBox9.Text, (Convert.ToString(numericUpDown10.Value)),textBox10.Text,(Convert.ToString(numericUpDown11.Value)),textBox11.Text};
+            System.IO.File.WriteAllLines(@textBox12.Text, to_write);
+            return;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[] to_read;
+            try
+            {
+                to_read = System.IO.File.ReadAllLines(@textBox12.Text);
+            }
+            catch(System.IO.FileNotFoundException)
+            {
+                return;
+            }
+            try
+            {
+                numericUpDown1.Value = Convert.ToInt32(to_read[0]);
+                textBox1.Text = to_read[1];
+                numericUpDown2.Value = Convert.ToInt32(to_read[2]);
+                textBox2.Text = to_read[3];
+                numericUpDown3.Value = Convert.ToInt32(to_read[4]);
+                textBox3.Text = to_read[5];
+                numericUpDown4.Value = Convert.ToInt32(to_read[6]);
+                textBox4.Text = to_read[7];
+                numericUpDown5.Value = Convert.ToInt32(to_read[8]);
+                textBox5.Text = to_read[9];
+                numericUpDown6.Value = Convert.ToInt32(to_read[10]);
+                textBox6.Text = to_read[11];
+                numericUpDown7.Value = Convert.ToInt32(to_read[12]);
+                textBox7.Text = to_read[13];
+                numericUpDown8.Value = Convert.ToInt32(to_read[14]);
+                textBox8.Text = to_read[15];
+                numericUpDown9.Value = Convert.ToInt32(to_read[16]);
+                textBox9.Text = to_read[17];
+                numericUpDown10.Value = Convert.ToInt32(to_read[18]);
+                textBox10.Text = to_read[19];
+                numericUpDown11.Value = Convert.ToInt32(to_read[20]);
+                textBox11.Text = to_read[21];
+            }
+            catch(IndexOutOfRangeException)
+            {
+                return;
+            }
+            return;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            AboutBox1 box = new AboutBox1();
+            box.ShowDialog();
+        }
+    }
+}
